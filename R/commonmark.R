94c223707212ed035496567f424c9f13493dcc65
Duncan Murdoch <murdoch.duncan@gmail.com>
2022-11-22 08:39:14 -0500

    Minimal changes to pass check.

--- R/commonmark.R
+++ R/commonmark.R
@@ -1,14 +1,14 @@
 fix_pandoc_from_options <- function(from, sourcepos) {
   from <- sub("^markdown", "commonmark", from)
   from <- sub("[+]tex_math_single_backslash", "", from)
-  from <- paste0(from, 
-                 "+yaml_metadata_block", 
+  from <- paste0(from,
+                 "+yaml_metadata_block",
                  if (sourcepos) "+sourcepos")
   from
 }
 
 html_commonmark_document <- function(sourcepos = TRUE, ...) {
-  res <- rmarkdown::html_document(...)
+  res <- html_document(...)
   res$pandoc$from <- fix_pandoc_from_options(res$pandoc$from, sourcepos)
 #  browser()
   res
@@ -16,7 +16,7 @@ html_commonmark_document <- function(sourcepos = TRUE, ...) {
 
 
 pdf_commonmark_document <- function(sourcepos = TRUE, ...) {
-  res <- rmarkdown::pdf_document(...)
+  res <- pdf_document(...)
   res$pandoc$from <- fix_pandoc_from_options(res$pandoc$from, sourcepos)
   res
 }
