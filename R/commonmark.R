7d67b1dd46f8b46b186e567d4e31b2ac10c83bf6
Duncan Murdoch <murdoch.duncan@gmail.com>
2022-11-25 14:23:22 -0500

    First pass at handling pdf_document.

--- R/commonmark.R
+++ R/commonmark.R
@@ -34,6 +34,32 @@ html_vignette_with_concordance <- html_with_concordance(html_vignette)
 
 pdf_document_with_concordance <- function(sourcepos = TRUE, ...) {
   res <- pdf_document(...)
-  res$pandoc$from <- fix_pandoc_from_options(res$pandoc$from, sourcepos)
+  if (sourcepos) {
+    res$pandoc$from <- fix_pandoc_from_options(res$pandoc$from, sourcepos)
+    res$pandoc$lua_filters <- c(res$pandoc$lua_filters,
+                                system.file("rmarkdown/lua/latex-datapos.lua", package = "RmdConcord"))
+    res$pandoc$ext = ".tex"
+    res$knitr$opts_knit$concordance <- sourcepos
+    res$knitr$opts_knit$out_format <- "markdown"
+    pdfengine <- which(res$pandoc$args == "--pdf-engine") + 1
+    oldpost <- res$post_processor
+    res$post_processor <- function(yaml, infile, outfile, ...) {
+      concordanceFile <- paste0(tools::file_path_sans_ext(infile),
+                                "-concordance.tex")
+      processLatexConcordance(outfile, followConcordance = concordanceFile)
+      args <- c("-synctex=1", outfile)
+      system2(res$pandoc$args[pdfengine], args)
+      synctexfile <- paste0(tools::file_path_sans_ext(outfile),
+                            ".synctex")
+      patchDVI::patchSynctex(synctexfile)
+      newoutfile <- paste0(tools::file_path_sans_ext(outfile),
+                           ".pdf")
+      if (is.function(oldpost))
+        res <- oldpost(yaml, infile, newoutfile, ...)
+      else
+        res <- newoutfile
+      res
+    }
+  }
   res
 }
